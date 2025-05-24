3f836bca0871f479ada190490c6522d475a8c8ae
Usernamecannotbeblank2 <isaac.dias1595@gmail.com>
2025-05-24 15:40:52 +0000

    - added emoji to title - changed input descriptions to a simpler format - Restructured order, added sub headers for sections - Changed max and min values for integers to fit the range from dataset - set starting values close to mean - internet options only show when the user says they have pay for the internet service - corrected tenture to tenure - formatted charges section to 2 decimals - reformatted production data - added line for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 
-st.title("Customer Churn Predictor 🚶‍♂️‍➡️")
+st.title("Customer Churn Predictor 🚪🚶‍♂️‍➡️")
 st.markdown("---")
 
 try:
@@ -11,113 +11,126 @@ except Exception as e:
     st.error(f'ERROR: {e}')
     st.stop()
 
+st.subheader("General info")
 
 gender = st.selectbox(
     "Gender:",
     ["Male", "Female"]
 )
 
-isSeniorCitizen = st.checkbox("Are you a senior citizen?")
-
-isPartner = st.checkbox("Do you have a partner?")
-
-isDependants = st.checkbox("Do you have dependants?")
-
-tenture = st.number_input(
-    "Tenture:",
+tenure = st.number_input(
+    "Tenure:",
     min_value=0,
     max_value=100,
-    value=50,
-    step=1
+    value=30,
+    step=10
 )
 
-isPhoneService = st.checkbox("Are we your phone provider?")
+isSeniorCitizen = st.checkbox("Senior citizen")
+
+isPartner = st.checkbox("Has partner")
+
+isDependants = st.checkbox("Dependents")
+
+st.markdown("---")
+st.subheader("Phone Service")
+
+isPhoneService = st.checkbox("Uses our phone service")
 
 isMultipleLines = False
 if(isPhoneService):
-    isMultipleLines = st.checkbox("Do you have more than one line with us?")
+    isMultipleLines = st.checkbox("More than one line")
+
+st.markdown("---")
+st.subheader("Internet Service")
 
 internetService = st.selectbox(
-    "Do you use our internet service?",
-    ["Yes, wireless", "Yes, fiber optic", "No"]
+    "Uses our internet service",
+    ["Yes, DSL", "Yes, fiber optic", "No"],
+    index=2 
 )
 
 isOnlineSecurity = False
 isOnlineBackup = False
+isDeviceProtected = False
+isTechSupport = False
+isStreamingTV = False
+isStreamingMovies = False
 if(internetService != "No"):
-    isOnlineSecurity = st.checkbox("Do you use our online security service?")
-    isOnlineBackup = st.checkbox("Do you use our online backup service?")
+    isOnlineSecurity = st.checkbox("Online security service")
+    isOnlineBackup = st.checkbox("Online backup service")
+    isDeviceProtected = st.checkbox("Device protection")
+    isTechSupport = st.checkbox("Technical support")
+    isStreamingTV = st.checkbox("TV streaming service")
+    isStreamingMovies = st.checkbox("Movie streaming service")
 
-isDeviceProtected = st.checkbox("Do you have device protection?")
+st.markdown("---")
+st.subheader("Billing")
 
-isTechSupport = st.checkbox("Do you have technical support?")
-
-isStreamingTV = st.checkbox("Do you have our TV streaming service?")
-isStreamingMovies = st.checkbox("Do you have our movie streaming service?")
+isPaperless = st.checkbox("Paperless billing")
 
 contract = st.selectbox(
-    "Contract Period:",
+    "Contract period type",
     ["Month-to-month", "One year", "Two year"]
 )
 
-isPaperless = st.checkbox("Do you use paperless billing?")
-
 paymentMethod = st.selectbox(
-    "What is your method of payment:",
+    "Payment method",
     ["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"]
 )
 
 monthlyCharges = st.number_input(
-    "Monthly Charges:",
-    min_value=0,
-    max_value=500,
-    value=50,
-    step=1
+    "Monthly Charges",
+    min_value=0.00,
+    max_value=200.00,
+    value=50.00,
+    step=10.00,
+    format="%.2f"
 )
 
-
 totalCharges = st.number_input(
-    "Total Charges:",
-    min_value=0,
-    max_value=500,
-    value=50,
-    step=1
+    "Total Charges",
+    min_value=0.00,
+    max_value=9000.00,
+    value=2000.00,
+    step=100.00,
+    format="%.2f"
 )
 
 if st.button("Process"):
-    #SHAPE -> ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges', 'gender_Male', 'Partner_Yes', 'Dependents_Yes', 'Contract_One year', 'Contract_Two year', 'PaperlessBilling_Yes', 'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check', 'PhoneService_Yes', 'MultipleLines_Yes', 'InternetService_Fiber optic', 'InternetService_No', 'OnlineSecurity_Yes', 'OnlineBackup_Yes', 'DeviceProtection_Yes', 'TechSupport_Yes', 'StreamingTV_Yes', 'StreamingMovies_Yes']
+    # SHAPE ORDER -> ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges', 'gender_Male', 'Partner_Yes', 'Dependents_Yes', 'Contract_One year', 'Contract_Two year', 'PaperlessBilling_Yes', 'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check', 'PhoneService_Yes', 'MultipleLines_Yes', 'InternetService_Fiber optic', 'InternetService_No', 'OnlineSecurity_Yes', 'OnlineBackup_Yes', 'DeviceProtection_Yes', 'TechSupport_Yes', 'StreamingTV_Yes', 'StreamingMovies_Yes']
     data = [
-        1 if isSeniorCitizen else 0,
-        1 if tenture else 0,
-        1 if monthlyCharges else 0,
-        1 if totalCharges else 0,
+        isSeniorCitizen,
+        tenure,
+        monthlyCharges,
+        totalCharges,
         1 if gender == 'Male' else 0,
-        1 if isPartner else 0,
-        1 if isDependants else 0,
+        isPartner,
+        isDependants,
         1 if contract == 'One year' else 0,
         1 if contract == 'Two year' else 0,
-        1 if isPaperless else 0,
+        isPaperless,
         1 if paymentMethod == 'Credit card (automatic)' else 0,
         1 if paymentMethod == 'Electronic check' else 0,
         1 if paymentMethod == 'Mailed check' else 0,
-        1 if isPhoneService else 0,
-        1 if isMultipleLines else 0,
+        isPhoneService,
+        isMultipleLines,
         1 if internetService == 'Yes, fiber optic' else 0,
         1 if internetService == 'No' else 0,
-        1 if isOnlineSecurity else 0,
-        1 if isOnlineBackup else 0,
-        1 if isDeviceProtected else 0,
-        1 if isTechSupport else 0,
-        1 if isStreamingTV else 0,
-        1 if isStreamingMovies else 0
+        isOnlineSecurity,
+        isOnlineBackup,
+        isDeviceProtected,
+        isTechSupport,
+        isStreamingTV,
+        isStreamingMovies
     ]
 
     data = np.array([data])
     prediction = model.predict(data)
 
     st.subheader("Prediction:")
+    st.text(prediction[0][0]) # for testing 
     if prediction[0][0] > 0.5:
         st.write("the user will be staying with the company 😄")
     else:
         st.write("the user will be leaving us 😔")
-
